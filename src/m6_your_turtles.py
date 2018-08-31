ea3c37746a0965ae36a150d516f3c49b759e88ff
kuznicmd <kuznicmd@rose-hulman.edu>
2018-08-31 13:52:46 -0400

    Version 2

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -28,4 +28,79 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 ###############################################################################
 
+import rosegraphics as rg
 
+window = rg.TurtleWindow()
+window.delay(10)
+
+alfred = rg.SimpleTurtle('turtle')
+alfred.pen_up()
+alfred.left(140)
+alfred.forward(450)
+alfred.right(140)
+
+rachel = rg.SimpleTurtle('arrow')
+rachel.pen_up()
+rachel.right(40)
+rachel.forward(450)
+rachel.right(140)
+
+jerry = rg.SimpleTurtle('square')
+jerry.pen_up()
+jerry.left(40)
+jerry.forward(450)
+jerry.right(130)
+
+claire = rg.SimpleTurtle('circle')
+claire.pen_up()
+claire.right(140)
+claire.forward(450)
+claire.right(130)
+
+alfred.pen = rg.Pen('red', 5)
+rachel.pen = rg.Pen('blue', 5)
+jerry.pen = rg.Pen('green', 5)
+claire.pen = rg.Pen('purple', 5)
+alfred.pen_down()
+rachel.pen_down()
+jerry.pen_down()
+claire.pen_down()
+
+alfred.speed = 20
+
+for k in range(10):
+
+    for k in range(4):
+
+        alfred.forward(500 - k * 25)
+        alfred.right(90)
+
+
+rachel.speed = 20
+
+for k in range(10):
+
+    for k in range(4):
+
+        rachel.forward(500 - k * 25)
+        rachel.right(90)
+
+jerry.speed = 20
+
+for k in range(10):
+
+    for k in range(4):
+
+        jerry.forward(500 - k * 25)
+        jerry.right(90)
+
+claire.speed = 20
+
+for k in range(10):
+
+    for k in range(4):
+
+        claire.forward(500 - k * 25)
+        claire.right(90)
+
+window.close_on_mouse_click()
